f410956772801a56336d87a2867bd3344fe92821
ujwal kumar <ujwal.professionaldev@gmail.com>
2023-11-22 20:20:56 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header/Header';
+import Main from './Main-Section/Main';
+import Navibar from './Navibar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main-Section/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Main.css"
+
+function Main() {
+  return (
+    <div className='Main-Section'>
+        <h1> MEDI-SCAN</h1>
+        <p> A universal medical report analysis platform.</p>
+    </div>
+  )
+}
+
+export default Main
